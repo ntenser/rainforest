cf64582c9ac563d098cc279d160c17493ebf235b
noah tenser <ntenser@hotmail.com>
2017-02-06 16:05:14 -0500

    Writing the review routes, controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
+  resources :products do
+    resources :reviews, only: [:show, :create, :destroy]
 
   resources :products
   resources :sessions, only: [:new, :create, :destroy]
