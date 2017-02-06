99f047950251b59454c955336e00f7a57b10dc4e
noah tenser <ntenser@hotmail.com>
2017-02-06 16:20:24 -0500

    Adding user names

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+
   has_many :reviews
   has_many :products, through: :reviews
-  
+
   has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :products do
     resources :reviews, only: [:show, :create, :destroy]
+  end
 
   resources :products
   resources :sessions, only: [:new, :create, :destroy]
